ab1eb22c61d965aca6a21af0d9f92c0b427a3258
SecVerg <75022571+SecVerg@users.noreply.github.com>
2020-11-25 20:37:10 +0100

    Added classes myGateway and myPacket. Also several functions added for calculations on packets like time on air and bitrate. Time on air function is not fully right yet, as it has a different outcome compared to online airtime calculators. Next step is to fix this issue.

--- LoRaSimSODAQ.py
+++ LoRaSimSODAQ.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Nov 23 14:59:04 2020
 
-@author: secve
+@author: secverg
 """
 
 import simpy
@@ -13,208 +13,160 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+global ax
+global nodes
+
 #
 # this function creates a node
 #
+
+
 class myNode():
-    def __init__(self, nodeid, bs, period, packetlen):
+    def __init__(self, nodeid, TXp, CF):
         self.nodeid = nodeid
-        self.period = period
-        self.bs = bs
         self.x = random.randint(0, 500)
         self.y = random.randint(0, 500)
-        self.dist = []
-
-        global nodes
-        
-        #self.packet = myPacket(self.nodeid, packetlen, self.dist)
+        self.packetList = []
+        self.distanceList = []
         self.sent = 0
+        self.received = 0
+        self.TXpower = TX[TXp]
+        self.energyUsed = 0
+        self.numberOfHops = 0
+        self.carrierFrequency = CF
 
         # graphics for node
         global graphics
         if (graphics == 1):
-            global ax
-            ax.add_artist(plt.Circle((self.x, self.y), 2, fill=True, color='blue'))
-                
+            self.graphic = plt.Circle((self.x, self.y), 2, fill=True, color='blue')
+
+        self.packetList.append(myPacket(random.randint(1, 51),\
+                                        random.randint(7, 12),\
+                                        4/5,\
+                                        random.choice(BW),\
+                                        0,\
+                                        0))
+
     def printInfo(self):
         print("NodeID", self.nodeid)
         print("node x: ", self.x)
         print("node y: ", self.y)
-        print("Distances: ", end= "")
-        print(*self.dist, sep= ", ")
-        print()
-        
+        print("Distances: ", end = "")
+        print(*self.distanceList, sep = ", ")
+
+
     def calcDist(self, nodes):
         for i in range(len(nodes)):
-            #print(i)
             if nodes[i].nodeid != self.nodeid:
-                nodeDist = []
-                xdist = self.x - nodes[i].x
-                ydist = self.y - nodes[i].y
+                nodeDist=[]
+                xdist=self.x - nodes[i].x
+                ydist=self.y - nodes[i].y
                 nodeDist.append(nodes[i].nodeid)
-                nodeDist.append(np.sqrt(xdist*xdist + ydist*ydist))
-                self.dist.append(nodeDist)
-        
+                nodeDist.append(np.sqrt(xdist * xdist + ydist * ydist))
+                self.distanceList.append(nodeDist)
 
-        
-            
-#
-# # this function creates a packet (associated with a node)
-# # it also sets all parameters, currently random
-# #
-# class myPacket():
-#     def __init__(self, nodeid, plen, distance):
-#         global experiment
-#         global Ptx
-#         global gamma
-#         global d0
-#         global var
-#         global Lpld0
-#         global GL
+    def sendPacket(self):
+        self.packetList[0].printInfo()
+        bitRate = self.packetList[0].calcBitRate()
+        Npayload = self.packetList[0].calcPayload()
+        print("Bitrate of packet (bits/s): ", bitRate)
+        print("Payload of packet (bytes): ", Npayload)
 
-#         self.nodeid = nodeid
-#         self.txpow = Ptx
-
-#         # randomize configuration values
-#         #self.sf = random.choice([12,11,11,10,10,10,10,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8])
-#         #self.sf = random.choice([8,9,10,11,12])
-#         #self.sf = random.choice([8,8,8,9,9,9,10,10,11,12])
-#         #self.sf = random.choice([12,11,11,10,10,10,9,9,9,9,8,8,8,8,8])
-#         #self.sf  = random.choice([8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,10,10,10,10,10,11,11,11,12])
-# #        self.sf  = random.choice([8,8,8,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,10,10,10,10,11,11,12])
-#         self.cr = random.randint(1,4)
-#         #self.bw = random.choice([125, 250, 500])
-        
-
-#         # for certain experiments override these
-#         if experiment==1 or experiment == 0:
-#             self.sf = 12
-#             self.cr = 4
-#             self.bw = 125
-
-#         # for certain experiments override these
-#         if experiment==2:
-#             self.sf = 6
-#             self.cr = 1
-#             self.bw = 500
-#         # lorawan
-#         if experiment == 4:
-#             self.sf = 12
-#             self.cr = 1
-#             self.bw = 125
+    def calcTOA(self, packet):
+        Tsymbol = (2**packet.SF) / packet.BW
+        Tpreamble = (4.25 + packet.Npreamble) * Tsymbol
+        Tpayload = packet.Npayload * Tsymbol
+        TOA = Tpayload + Tpreamble
+        print("Time on air for packet number ", self.packetList.index(packet), ": ", TOA)
+        print()
 
 
-#         # for experiment 3 find the best setting
-#         # OBS, some hardcoded values
-#         Prx = self.txpow  ## zero path loss by default
 
-#         # log-shadow
-#         Lpl = Lpld0 + 10*gamma*math.log(distance/d0)
-#         print(("Lpl:", Lpl))
-#         Prx = self.txpow - GL - Lpl
+class myGateway():
+    def __init__(self, gateID):
+        self.gateID = gateID
+        self.receivedPackets = 0
+        self.numberOfHops = 0
+        self.distanceList = []
 
-#         if (experiment == 3) or (experiment == 5):
-#             minairtime = 9999
-#             minsf = 0
-#             minbw = 0
+class myPacket():
+    def __init__(self, packetLength, spreadingFactor, codingRate, bandwidth, header, lowDataRateOpt):
+        self.PL = packetLength
+        self.SF = spreadingFactor
+        self.CR = codingRate
+        self.BW = bandwidth
+        self.Npreamble = 8
+        self.header = header
+        self.lowDataRateOpt = lowDataRateOpt
 
-#             print(("Prx:", Prx))
-#             self.cr = 1
-#             self.bw = 125
-#             self.sf  = random.choice([7,7,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,9,9,9,9,10,10,11,12])        
-#             #self.sf = random.choice([12,11,11,10,10,10,10,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8])
-#             #self.sf = random.choice([8,9,10,11,12])
-#             #self.sf = random.choice([8,8,8,9,9,9,10,10,11,12])
-#             #self.sf = random.choice([12,11,11,10,10,10,9,9,9,9,8,8,8,8,8])
-#             #self.sf  = random.choice([8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,10,10,10,10,10,11,11,11,12])
-#             #self.sf  = random.choice([8,8,8,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,10,10,10,10,11,11,12])
+    def printInfo(self):
+        print("PL: ", self.PL)
+        print("SF: ", self.SF)
+        print("CR: ", self.CR)
+        print("BW: ", self.BW)
 
-#             at = airtime(self.sf, self.cr, plen, self.bw)
-#             if at < minairtime:
-#                 minairtime = at
-#                 minsf = self.sf
-#                 minbw = self.bw
-#                 minsensi = -88
-#             if (minairtime == 9999):
-#                 print("does not reach base station")
-#                 sys.exit(-1)
-#             print(("best sf:", minsf, " best bw: ", minbw, "best airtime:", minairtime))
-#             self.rectime = minairtime
-#             self.bw = minbw
-#             self.cr = 1
+    def calcBitRate(self):
+        self.bitRate = self.SF * ((self.BW / (2**self.SF)) * self.CR)
+        return self.bitRate
 
-#             if experiment == 5:
-#                 # reduce the txpower if there's room left
-#                 self.txpow = max(2, self.txpow - math.floor(Prx - minsensi))
-#                 Prx = self.txpow - GL - Lpl
-#                 print(('minsesi {} best txpow {}'.format(minsensi, self.txpow)))
+    def calcPayload(self):
+        thetaPLSF = 8 * self.PL - 4 * self.SF + 16 + 28 - 20 * self.header
+        gammaSF = self.SF - 2 * self.lowDataRateOpt
+        self.Npayload = 8 + max(math.ceil(thetaPLSF/gammaSF)*(1/self.CR), 0)
+        return self.Npayload
 
-#         # transmission range, needs update XXX
-#         self.transRange = 150
-#         self.pl = plen
-#         self.symTime = (2.0**self.sf)/self.bw
-#         self.arriveTime = 0
-#         self.rssi = Prx
-#         # frequencies: lower bound + number of 61 Hz steps
-#         #self.freq = 860000000 + random.randint(0,2622950)
-
-#         # for certain experiments override these and
-#         # choose some random frequences
-        
-#         self.freq = random.choice([868100000, 868300000, 868500000,868700000,868900000,869100000,869300000,869500000])
-#         #self.freq = random.choice([866500000,866700000,866900000,867100000,867300000,867500000,867700000,867900000,868100000, 868300000, 868500000,868700000,868900000,869100000,869300000,869500000])
-
-#         print(("frequency" ,self.freq, "symTime ", self.symTime))
-#         print(("bw", self.bw, "sf", self.sf, "cr", self.cr, "rssi", self.rssi))
-#         self.rectime = airtime(self.sf,self.cr,self.pl,self.bw)
-#         print(("rectime node ", self.nodeid, "  ", self.rectime))
-#         # denote if packet is collided
-#         self.collided = 0
-#         self.processed = 0
 
 # 1 to show nodes in plot
-graphics = 1
-
-# 868 MHz EU Carrier frequency
-freq = 0.868
+graphics=1
 
 # list for all nodes
-nodes = []
+nodes=[]
+
+# list for available bandwidths
+#BW = [7.8, 10.4, 15.6, 20.8, 31.2, 41.7, 62.5, 125, 250, 500]
+BW = [125000]
+
+# compute energy
+# Transmit consumption in mA from -2 to +17 dBm
+TX = [22, 22, 22, 23,                                      # RFO/PA0: -2..1
+      24, 24, 24, 25, 25, 25, 25, 26, 31, 32, 34, 35, 44,  # PA_BOOST/PA1: 2..14
+      82, 85, 90,                                          # PA_BOOST/PA1: 15..17
+      105, 115, 125]                                       # PA_BOOST/PA1+PA2: 18..20
+# mA = 90    # current draw for TX = 17 dBm
+V = 3.0     # voltage XXX
 
 # get arguments
 if len(sys.argv) >= 3:
-    nrNodes = int(sys.argv[1])
-    avgSendTime = int(sys.argv[2])
-    PL = int(sys.argv[3])
-    
-    print("Nodes: ", nrNodes)
-    print("AvgSendTime (exp. distributed): ",avgSendTime)
-    print("PacketLength: ",PL)
+    nrNodes=int(sys.argv[1])
+    TXpowerArg=int(sys.argv[2])
+    carrierFrequency=int(sys.argv[3])
+
+    print("Number of nodes: ", nrNodes)
+    print("TXpower: ", TXpowerArg)
+    print("CarrierFrequency: ", carrierFrequency)
+
 else:
-    print("usage: ./loraDir <nodes> <avgsend> <PacketLength>")
+    print("usage: ./loraDir <amount of nodes> <TXpower> <carrierFrequency>")
     sys.exit(-1)
 
-#prepare show
-if (graphics == 1):
-    plt.ion()
-    plt.figure()
-    ax = plt.gcf().gca()
-    
-# Create nodes with avgSendTime and PacketLength 
+# Create nodes with avgSendTime and PacketLength
 # and add new nodes to nodes list
-for i in range(0,nrNodes):
-    node = myNode(i, 0, avgSendTime, PL)
+for i in range(0, nrNodes):
+    node=myNode(i, TXpowerArg, carrierFrequency)
     nodes.append(node)
-    
+
 # Calculate distance between nodes and print node info of all nodes
 for i in range(len(nodes)):
     nodes[i].calcDist(nodes)
     nodes[i].printInfo()
+    nodes[i].sendPacket()
+    nodes[i].calcTOA(nodes[i].packetList[0])
 
-# Show plot
+# prepare show
 if (graphics == 1):
-    plt.xlim([0, 500])
-    plt.ylim([0, 500])
-    plt.draw()
+    fig, ax=plt.subplots()
+    ax.set_xlim((0, 500))
+    ax.set_ylim((0, 500))
+    for i in range(len(nodes)):
+        plt.gcf().gca().add_artist(nodes[i].graphic)
     plt.show()
-    
